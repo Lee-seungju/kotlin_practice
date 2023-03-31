dbb8b58cba45e208d5970b80a62cdbad8ca51539
seungju.lee <seungju.lee@sisoul.co.kr>
2023-03-31 14:11:14 +0900

    Add Random Dice App

---
+++ dice_app/app/src/main/java/com/slee2/dice_app/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.slee2.dice_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.slee2.dice_app.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val diceImage1 = binding.dice1
+        val diceImage2 = binding.dice2
+
+        binding.diceStartBtn.setOnClickListener {
+
+            Toast.makeText(this, "주사위 GO!",  Toast.LENGTH_LONG).show()
+
+            val first = Random.nextInt(1, 6)
+            val second = Random.nextInt(1, 6)
+
+            when (first) {
+                1 -> diceImage1.setImageResource(R.drawable.dice_1)
+                2 -> diceImage1.setImageResource(R.drawable.dice_2)
+                3 -> diceImage1.setImageResource(R.drawable.dice_3)
+                4 -> diceImage1.setImageResource(R.drawable.dice_4)
+                5 -> diceImage1.setImageResource(R.drawable.dice_5)
+                6 -> diceImage1.setImageResource(R.drawable.dice_6)
+            }
+
+            when (second) {
+                1 -> diceImage2.setImageResource(R.drawable.dice_1)
+                2 -> diceImage2.setImageResource(R.drawable.dice_2)
+                3 -> diceImage2.setImageResource(R.drawable.dice_3)
+                4 -> diceImage2.setImageResource(R.drawable.dice_4)
+                5 -> diceImage2.setImageResource(R.drawable.dice_5)
+                6 -> diceImage2.setImageResource(R.drawable.dice_6)
+            }
+        }
+    }
+}
